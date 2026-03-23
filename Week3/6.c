d1cadac089e66df28aa0e12301ec995da07f29bc
user <user@PC-23>
2026-03-23 16:51:14 +0900

    config

---
+++ Week3/6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+	const float PI = 3.141592;
+	float radius = 10.0;
+	float area;
+
+	area = PI * radius * radius;
+	printf("circle : %f", area);
+
+	return 0;
+
+}
