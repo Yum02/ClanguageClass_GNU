16f055c0e9157ef4f97b27842a4c3bc90f4d7474
Yum02 <chamch135@naver.com>
2026-05-18 18:24:35 +0900

    3

--- Week10/15.c
+++ Week10/15.c
@@ -3,8 +3,13 @@
 
 int main()
 {
-	int i;
+	int s[5], i;
+	printf("정수 5개 입력 : ");
+	for (i = 0; i < 5; i++)
+		scanf("%d", &s[i]);
 
+	for (i = 0; i < 5; i++)
+		printf("%d\n", s[i]);
 
 	return 0;
 }
