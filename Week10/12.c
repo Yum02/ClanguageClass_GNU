0e47af0cf53212c93fc2c71051638621c80eb645
Yum02 <chamch135@naver.com>
2026-05-18 18:14:37 +0900

    1

--- Week10/12.c
+++ Week10/12.c
@@ -3,8 +3,27 @@
 
 int main()
 {
-	int i;
-
+	double uc, cg = 3000.0;
+	printf("현재 남아있는 충전량 : %lf\n", cg);
+	for (;;)
+	{
+		printf("충전<+>/사용<-> : ");
+		scanf("%lf", &uc);
+		cg = cg + uc;
+		printf("현재 남아있는 충전량 : %lf\n", cg);
+		if (cg <= 300 && cg > 0)
+		{
+			printf("Warrning!! 충전량이 300이하이므로 충전하여 주세요.\n");
+			continue;
+		}
+		else if (cg > 300)
+			continue;
+		else
+		{
+			printf("충전량이 0이므로 꺼집니다.");
+			break;
+		}
+	}
 
 	return 0;
 }
--- Week10/14.c
+++ Week10/14.c
@@ -4,7 +4,17 @@
 int main()
 {
 	int i;
+	int s[5] = {0};
 
+	for (i = 0; i < 5; i++)
+	{
+		printf("점수 입력 : ");
+		scanf(" %d",&s[i]);
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%2d", s[i]);
+	}
 
 	return 0;
 }
